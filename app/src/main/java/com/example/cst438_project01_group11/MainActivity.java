4d6ae9a6ff935eca8a5e7b7da5151f313a081422
Jasdeep Singh Dhillon <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2021-09-02 15:07:15 +0530

    Implemented bottom navigation and basic fragments

--- app/src/main/java/com/example/cst438_project01_group11/MainActivity.java
+++ app/src/main/java/com/example/cst438_project01_group11/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.cst438_project01_group11;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView mBottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager()
+            .beginTransaction()
+            .replace(R.id.fragment_container, new PokedexFragment())
+            .commit();
+
+        setBottomNavigationListener();
     }
+
+    private void setBottomNavigationListener() {
+        mBottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+
+                Fragment selectedFragment;
+
+                if(item.getItemId() == R.id.pokedex_navigation) {
+                    selectedFragment = new PokedexFragment();
+                } else if ( item.getItemId() == R.id.poketeam_navigation) {
+                    selectedFragment = new TeamFragment();
+                } else if( item.getItemId() == R.id.random_pokemon_navigation) {
+                    selectedFragment = new RandomPokemonFragment();
+                } else {
+                    return false;
+                }
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+                return true;
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/example/cst438_project01_group11/PokedexFragment.java
@@ -0,0 +1,20 @@
+package com.example.cst438_project01_group11;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class PokedexFragment extends Fragment {
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @org.jetbrains.annotations.NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.pokedex_layout, container, false);
+    }
+}
