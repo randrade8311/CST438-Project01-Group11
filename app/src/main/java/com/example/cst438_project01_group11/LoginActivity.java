adb33525baef12e8dd0043758d7872f8e64ab5b1
Aundre Labrador <aundrelabrador@gmail.com>
2021-09-01 22:47:08 -0700

    Created home page view with on-click buttons to login and sign up views

---
+++ app/src/main/java/com/example/cst438_project01_group11/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.cst438_project01_group11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+}
--- app/src/main/java/com/example/cst438_project01_group11/MainActivity.java
+++ app/src/main/java/com/example/cst438_project01_group11/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.cst438_project01_group11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // creating Buttons
+        Button buttonForManageSystem = findViewById(R.id.login);
+        Button buttonForCreateNewAccount = findViewById(R.id.create_account);
+
+        // creating on click listeners
+        buttonForManageSystem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+        buttonForCreateNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, CreateAccountActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
