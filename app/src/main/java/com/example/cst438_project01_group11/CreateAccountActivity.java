bf8e4c42b44f687069c4322b19e3dd8637c5918a
Aundre Labrador <aundrelabrador@gmail.com>
2021-09-08 23:54:25 -0700

    Added create account view and retrieved input. Also added blueprints for working with user database to validate input and add users.

--- app/src/main/java/com/example/cst438_project01_group11/CreateAccountActivity.java
+++ app/src/main/java/com/example/cst438_project01_group11/CreateAccountActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
         import android.os.Bundle;
         import android.view.View;
         import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class CreateAccountActivity extends AppCompatActivity {
@@ -15,7 +17,40 @@ public class CreateAccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
-    }
+        Button submitBtn = findViewById(R.id.submit);
+        submitBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View c) {
+                // Input Values
+                EditText name1 = findViewById(R.id.name);
+                EditText username1 = findViewById(R.id.username);
+                EditText password1 = findViewById(R.id.password);
+                String name = name1.getText().toString();
+                String username = username1.getText().toString();
+                String password = password1.getText().toString();
 
+                // Check if fields are not empty, else
+                if(username.equals("") || password.equals("")){
+                    Toast.makeText(CreateAccountActivity.this, "Empty Field(s)", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    // Check if User is in DB using username search
+                    if(false){ // Create new User instance and add to DB
+                        Toast.makeText(CreateAccountActivity.this, "Account Created", Toast.LENGTH_LONG).show();
+                    }
+                    else{ // Username was taken
+                        Toast.makeText(CreateAccountActivity.this, "Username taken", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+
+        Button backBtn = findViewById(R.id.back);
+        backBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View c){finish();}
+        });
+    }
 }
 
--- app/src/main/java/com/example/cst438_project01_group11/LoginActivity.java
+++ app/src/main/java/com/example/cst438_project01_group11/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -22,8 +23,23 @@ public class LoginActivity extends AppCompatActivity {
         buttonForManageSystem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Input Values
+                EditText username1 = findViewById(R.id.username);
+                EditText password1 = findViewById(R.id.password);
+                String username = username1.getText().toString();
+                String password = password1.getText().toString();
+
+                /* Validation */
+                // Iterate over list of Users in DB
+                //          Function: return list of users
+                // If username + password equals a User
+                //          Start LandingPageActivity --> Jasdeep's Home Page
+                //          Pass in a bundle with username + name;
                 Intent i = new Intent(LoginActivity.this, LoginActivity.class);
                 startActivity(i);
+                // else, display error message
+                //      or add functionality to determine whether username or password is wrong
+                //      and highlight text field
             }
         });
         buttonForCreateNewAccount.setOnClickListener(new View.OnClickListener() {
