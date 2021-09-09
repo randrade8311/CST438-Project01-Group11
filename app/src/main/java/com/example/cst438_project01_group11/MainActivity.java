d563e5c2852ef45fdaae484bbc2055b508d3f501
amemariee <awall@csumb.edu>
2021-09-09 10:24:26 -0700

    fixed retrofit error

--- app/src/main/java/com/example/cst438_project01_group11/MainActivity.java
+++ app/src/main/java/com/example/cst438_project01_group11/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
+import com.android.volley.Response;
 import com.example.cst438_project01_group11.models.Pokemon;
 import com.example.cst438_project01_group11.models.PokemonRes;
+import com.example.cst438_project01_group11.pokiapi.PokiapiService;
 
 import org.chromium.base.Callback;
 import org.chromium.base.Log;
@@ -34,29 +36,29 @@ public class MainActivity extends AppCompatActivity {
         obtenerDatos();
     }
     private void obtenerDatos(){
-       PokeapiService service = retrofit.create(PokeapiService.class);
+       PokiapiService service = retrofit.create(PokiapiService.class);
        Call<PokemonRes> pokemonResCall = service.obtenerListaPokemon();
 
        pokemonResCall.enqueue(new Callback<PokemonRes>()){
-           @Override
-           public void onResponse(Call<PokemonRes> call, Response<PokemonRes> response) {
-               if (response.isSuccessful()){
-                   PokemonRes pokemonRes = response.body();
-                   ArrayList<Pokemon> listaPokemon = pokemonRes.getResults();
-
-                   for(int i = 0; i < listaPokemon.size(); i++){
-                       Pokemon p = listaPokemon.get(i);
-                       Log.i(TAG, " Pokemon: " + p.getName());
-                   }
-               } else {
-                   Log.e(TAG, " onResponse: " + response.errorBody());
-               }
-           }
-           @Override
-           public void onFailure(Call<PokemonRes> call, Throwable t){
-               Log.e(TAG, " onFailure: " + t.getMessage())
-           }
-       });
+//           @Override
+//           public void onResponse(Call<PokemonRes> call, Response<PokemonRes> response) {
+//               if (response.isSuccessful()){
+//                   PokemonRes pokemonRes = response.body();
+//                   ArrayList<Pokemon> listaPokemon = pokemonRes.getResults();
+//
+//                   for(int i = 0; i < listaPokemon.size(); i++){
+//                       Pokemon p = listaPokemon.get(i);
+//                       Log.i(TAG, " Pokemon: " + p.getName());
+//                   }
+//               } else {
+//                   Log.e(TAG, " onResponse: " + response.errorBody());
+//               }
+//           }
+//           @Override
+//           public void onFailure(Call<PokemonRes> call, Throwable t){
+//               Log.e(TAG, " onFailure: " + t.getMessage())
+//           }
+//       });
 
 
     }
