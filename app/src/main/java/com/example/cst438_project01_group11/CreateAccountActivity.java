2232e14aa52d3f332deca82db3399db010d6a7f3
Aundre Labrador <aundrelabrador@gmail.com>
2021-09-15 23:54:31 -0700

    Added functionality for validating user login information and for inserting new users into database

--- app/src/main/java/com/example/cst438_project01_group11/CreateAccountActivity.java
+++ app/src/main/java/com/example/cst438_project01_group11/CreateAccountActivity.java
@@ -4,18 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
         import android.content.Intent;
         import android.os.Bundle;
-        import android.view.View;
+import android.util.Log;
+import android.view.View;
         import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
 public class CreateAccountActivity extends AppCompatActivity {
+    private PokedexDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        db = PokedexDatabase.getInstance(this);
 
         Button submitBtn = findViewById(R.id.submit);
         submitBtn.setOnClickListener(new View.OnClickListener(){
@@ -30,12 +33,18 @@ public class CreateAccountActivity extends AppCompatActivity {
                 String password = password1.getText().toString();
 
                 // Check if fields are not empty, else
-                if(username.equals("") || password.equals("")){
+                if(username.equals("") || password.equals("")) {
                     Toast.makeText(CreateAccountActivity.this, "Empty Field(s)", Toast.LENGTH_LONG).show();
                 }
                 else{
                     // Check if User is in DB using username search
-                    if(false){ // Create new User instance and add to DB
+                    User isInDb;
+                    isInDb = db.user().findByUsername(username);
+
+                    if(isInDb == null){ // Create new User instance and add to DB
+                        User user = new User(username, password);
+                        db.user().addUser(user);
+                        System.out.print("CREATED NEW USER!!!!!!");
                         Toast.makeText(CreateAccountActivity.this, "Account Created", Toast.LENGTH_LONG).show();
                     }
                     else{ // Username was taken
